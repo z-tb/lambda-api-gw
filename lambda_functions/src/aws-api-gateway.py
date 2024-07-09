5296a3e2e5638f4ed08043795ceb26e3a80fa208
tblader <tblader@wisc.edu>
2024-07-09 14:18:25 -0500

    bugfix: lambda should print valid json

--- lambda_functions/src/aws-api-gateway.py
+++ lambda_functions/src/aws-api-gateway.py
@@ -1,15 +1,22 @@
+#!/usr/bin/env python3
+import boto3
+import json
 
 def lambda_handler(event, context):
   print (f"Received event: {event}")
   
   statusCode = 200
-  returnBody = "Hello from Lambda"
+  returnBody = {"message": "Hello from Lambda"}
 
   # Return a response
   response = {
-        "statusCode": statusCode,
-        "body": returnBody
-    }
-      
+      "statusCode": statusCode,
+      "headers": {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*"  # For CORS support
+      },
+      "body": json.dumps(returnBody)
+  }
+    
   return response
 
